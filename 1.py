45eb1e188af43cadd1e905c40feea85a3d554fc1
umsito <30744803+umsito@users.noreply.github.com>
2017-08-05 00:06:24 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+n=int(raw_input('Enter the number'))
+m=int(raw_input('Enter the number'))
+l=int(raw_input('Enter the number'))
+if(n>m and l):
+     print n
+elif(m>n and l):
+     print m
+else:
+     print l
+
+
+
+
+
+for i in range(0,5):
+ print("hello")
+
